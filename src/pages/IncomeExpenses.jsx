f7a2d302ef32f84b24fe339d856bb339e34f4895
Qunoot Zehra <zehraqunoot93@gmail.com>
2026-09-21 23:24:53 +0530

    Add Income and Expenses page structure

---
+++ src/pages/IncomeExpenses.jsx
@@ -0,0 +1,10 @@
+function IncomeExpenses() {
+  return (
+    <div>
+      <h1>Income & Expenses</h1>
+      <p>Track your income and expenses here.</p>
+    </div>
+  );
+}
+
+export default IncomeExpenses;
